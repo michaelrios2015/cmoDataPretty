f38a44f8c2dc539c211194054461887c8346e0e1
Michael Rios <michael.rios093@gmail.com>
2021-04-26 14:53:06 -0400

    seeding??

--- server/index.js
+++ server/index.js
@@ -9,9 +9,9 @@ const init = async()=> {
         await syncAndSeed();
       }
       //no clue what this is doing but was in the Grace Shopper boiler plate should ask
-      else {
-        await db.sync()
-      }
+      // else {
+      //   await db.sync()
+      // }
       const port = process.env.PORT || 3000;
       app.listen(port, ()=> console.log(`listening on port ${port}`));
     }

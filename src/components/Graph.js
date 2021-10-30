68b60e4765d0ade02636cc318731d4a0d1f5ef07
Michael Rios <michael.rios093@gmail.com>
2021-10-30 18:05:27 -0400

    more graph data

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -2,26 +2,28 @@ import React, { Component } from 'react';
 import {Line} from 'react-chartjs-2';
 
 const state = {
-  labels: ['20', '25', '30', '35', '40', '45', ' 50', '55', '60', '65', '70', '75', '80'],
+  labels: ['0', '5', '10', '15', '20', '25', '30', '35', '40', '45', ' 50', '55', '60', '65', '70', '75', '80', '85' , '90' , '95'],
   datasets: [
     {
-      label: 'Rainfall',
+      label: 'G1 Pools',
       fill: false,
       lineTension: 0,
       backgroundColor: 'rgba(75,192,192,1)',
       borderColor: 'rgba(0,0,0,1)',
       borderWidth: 2,
-      data: [65, 59, 80, 81, 56]
-    },
-    {
-      label: 'RainPaul',
-      fill: false,
-      lineTension: 0,
-      backgroundColor: 'rgba(75,192,192,1)',
-      borderColor: 'rgba(0,0,0,1)',
-      borderWidth: 2,
-      data: [5, 59, 37, 81, 50]
+      data: [881225, 766401, 669811, 616160,  600036, 511758, 456201, 421243, 332102, 178385, 99819,
+        18805, 1668, 1243, 427, 108, 95, 94, 6, 1]
     }
+    // ,
+    // {
+    //   label: 'RainPaul',
+    //   fill: false,
+    //   lineTension: 0,
+    //   backgroundColor: 'rgba(75,192,192,1)',
+    //   borderColor: 'rgba(0,0,0,1)',
+    //   borderWidth: 2,
+    //   data: [5, 59, 37, 81, 50]
+    // }
   ]
 }
 

1134520fd4e564409dab1135af628155ff144f2b
Michael Rios <michael.rios093@gmail.com>
2021-10-31 19:59:59 -0400

    more graph data

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -5,6 +5,27 @@ const state = {
   labels: ['0', '5', '10', '15', '20', '25', '30', '35', '40', '45', ' 50', '55', '60', '65', '70', '75', '80', '85' , '90' , '95'],
   datasets: [
     {
+      label: 'G2 Pools, Coupon 4.5',
+      fill: false,
+      lineTension: 0,
+      backgroundColor: 'rgba(75,192,192,1)',
+      borderColor: 'rgba(0,0,0,1)',
+      borderWidth: 2,
+      data: [  34263, 34140, 34135, 34016, 33821, 30029, 25716, 24168, 23296, 21163, 9531, 1546, 82, 82, 82, 82, 82, 81 ]
+    }
+    ,
+    {
+      label: 'G2 Pools, Coupon 4.5 - Predicted CPR',
+      fill: false,
+      lineTension: 0,
+      backgroundColor: 'rgba(255, 99, 132, 0.2)',
+      // line color
+      borderColor: "#80b6f4",
+      borderWidth: 2,
+      data: [ 34263, 34157, 34157, 34155, 33916, 33590, 24375, 23689, 22018, 12635]
+    }, 
+  
+    {
       label: 'G2 Pools, Coupon 5',
       fill: false,
       lineTension: 0,
@@ -15,7 +36,7 @@ const state = {
     }
     ,
     {
-      label: 'Predicted CPR',
+      label: 'G2 Pools, Coupon 5 - Predicted CPR',
       fill: false,
       lineTension: 0,
       backgroundColor: 'rgba(255, 99, 132, 0.2)',

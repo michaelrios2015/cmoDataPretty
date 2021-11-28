a6d9706cda97fda8aa3e4be3a452b09085f51e2d
Michael Rios <michael.rios093@gmail.com>
2021-11-27 22:17:21 -0500

    ??

--- src/store/g1s.js
+++ src/store/g1s.js
@@ -37,51 +37,34 @@ const formatData = (arr) => {
 
     arr.forEach(item => {
 
-
-        // for (const property in item) {
-        //     if (item[property] * 1  == 0 || item[property] * 1 == -0){
-        //         item[property] = '';
-        //         console.log(`${property}: ${item[property]}`);
-        //     }    
-        // }
-
         item.currentface = (item.currentface/1000000).toFixed(1);
+        
         item.cfincmo = (item.cfincmo/1000000).toFixed(1);
+        
         item.cfinplat = (item.cfinplat/1000000).toFixed(1);
-        if (item.cfinfed){ 
-            item.cfinfed = (item.cfinfed/1000000).toFixed(1);
-        }
-        if (item.pastactcpr){ 
-            item.pastactcpr = (item.pastactcpr * 100).toFixed(1); 
-        }
-        // if (item.curractualcpr){ 
-            item.curractualcpr = (item.curractualcpr * 100).toFixed(1);
-        // }
-        if (item.cprprediction){ 
-            item.cprprediction = (item.cprprediction * 100).toFixed(1);
-        }
-        if (item.cprpredictionnext){ 
-            item.cprpredictionnext = (item.cprpredictionnext * 100).toFixed(1);
-        }
-        if (item.pastactcdr){ 
-            item.pastactcdr = (item.pastactcdr * 100).toFixed(1); 
-        }
-        if (item.curractualcdr){ 
-            item.curractualcdr = (item.curractualcdr * 100).toFixed(1);
-        }
-        if (item.cdrprediction){ 
-            item.cdrprediction = (item.cdrprediction * 100).toFixed(1);
-        }
-        if (item.cdrpredictionnext){ 
-            item.cdrpredictionnext = (item.cdrpredictionnext * 100).toFixed(1);
-        }
-        if (item.issuedate){ 
-            item.issuedate = item.issuedate.toString().slice(0, 4) + item.issuedate.toString().slice(5, 7);
-        }
-        if (item.va){ 
-            item.va = (item.va * 100).toFixed(0);
-        }    
-    
+        
+        item.cfinfed = (item.cfinfed/1000000).toFixed(1);
+        
+        item.pastactcpr = (item.pastactcpr * 100).toFixed(1); 
+        
+        item.curractualcpr = (item.curractualcpr * 100).toFixed(1);
+        
+        item.cprprediction = (item.cprprediction * 100).toFixed(1);
+        
+        item.cprpredictionnext = (item.cprpredictionnext * 100).toFixed(1);
+        
+        item.pastactcdr = (item.pastactcdr * 100).toFixed(1); 
+        
+        item.curractualcdr = (item.curractualcdr * 100).toFixed(1);
+        
+        item.cdrprediction = (item.cdrprediction * 100).toFixed(1);
+        
+        item.cdrpredictionnext = (item.cdrpredictionnext * 100).toFixed(1);
+        
+        item.issuedate = item.issuedate.toString().slice(0, 4) + item.issuedate.toString().slice(5, 7);
+        
+        item.va = (item.va * 100).toFixed(0);
+        
         for (const property in item) {
             if (item[property] * 1  == 0 || item[property] * 1 == -0){
                 item[property] = '';

4ebc413cce9a9e0b9d5b3443fb61b176a627396c
michaelrios2015 <michael.rios093@gmail.com>
2023-06-07 11:33:12 -0400

    may 4th day

--- data/changeme.js
+++ data/changeme.js
@@ -4,7 +4,7 @@ export const feddate = '5/31/23';
 // changed on the 6th day 
 export const month = 3; 
 // changed on the 4th and 6th day 
-export const is4thday = false;
+export const is4thday = true;
 
 // use this for the graph 
 // changed on the 6th day 

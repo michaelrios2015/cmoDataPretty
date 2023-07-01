2e0ea23749137abc7e5e1c3386b3cae3269f26b9
michaelrios2015 <michael.rios093@gmail.com>
2023-07-01 15:02:30 -0400

    fed date

--- data/changeme.js
+++ data/changeme.js
@@ -1,6 +1,6 @@
 // use these for cmo and ginnie tables 
 // whenever I change fed data 
-export const feddate = '6/07/23';
+export const feddate = '6/28/23';
 // changed on the 6th day 
 export const month = 4; 
 // changed on the 4th and 6th day 

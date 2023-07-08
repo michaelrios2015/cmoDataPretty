a67147d1e40b70f19629aa152d26a5c07c2661e6
michaelrios2015 <michael.rios093@gmail.com>
2023-07-07 21:42:19 -0400

    june intial cprs

--- data/changeme.js
+++ data/changeme.js
@@ -4,7 +4,7 @@ export const feddate = '6/28/23';
 // changed on the 6th day 
 export const month = 4; 
 // changed on the 4th and 6th day 
-export const is4thday = false;
+export const is4thday = true;
 
 // use this for the graph 
 // changed on the 6th day 

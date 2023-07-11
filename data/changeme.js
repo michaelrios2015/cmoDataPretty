c56dbf63aa142a35e0d98023a260d8b537364553
michaelrios2015 <michael.rios093@gmail.com>
2023-07-11 18:04:58 -0400

    june 6th day

--- data/changeme.js
+++ data/changeme.js
@@ -1,12 +1,12 @@
 // use these for cmo and ginnie tables 
 // whenever I change fed data 
-export const feddate = '6/28/23';
+export const feddate = '7/05/23';
 // changed on the 6th day 
-export const month = 4; 
+export const month = 5; 
 // changed on the 4th and 6th day 
-export const is4thday = true;
+export const is4thday = false;
 
 // use this for the graph 
 // changed on the 6th day 
-export const date = 'May 2023';
+export const date = 'June 2023';
 
--- data/servervar.js
+++ data/servervar.js
@@ -1,5 +1,5 @@
 // so I really vhang eboth at once???
 
-const currentMonth = '2023-05-01';
+const currentMonth = '2023-06-01';
 
 module.exports = currentMonth;

fbfdf17b2fd46de1f0c438752366883c7eb38e6b
michaelrios2015 <michael.rios093@gmail.com>
2023-09-03 13:31:17 -0400

    getting ready for august data

--- data/changeme.js
+++ data/changeme.js
@@ -1,6 +1,6 @@
 // use these for cmo and ginnie tables 
 // whenever I change fed data 
-export const feddate = '8/09/23';
+export const feddate = '8/30/23';
 // changed on the 6th day 
 export const month = 6; 
 // changed on the 4th and 6th day 

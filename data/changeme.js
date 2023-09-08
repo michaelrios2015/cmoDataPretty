870b8cbf2407eb7230e84ea966f08ac3eccaaba3
michaelrios2015 <michael.rios093@gmail.com>
2023-09-07 22:28:03 -0400

    sept 4th business day

--- data/changeme.js
+++ data/changeme.js
@@ -4,7 +4,7 @@ export const feddate = '8/30/23';
 // changed on the 6th day 
 export const month = 6; 
 // changed on the 4th and 6th day 
-export const is4thday = false;
+export const is4thday = true;
 
 // use this for the graph 
 // changed on the 6th day 

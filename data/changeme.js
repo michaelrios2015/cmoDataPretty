77123e769b6ae4844ca2372b654f9af8c87beaf1
michaelrios2015 <michael.rios093@gmail.com>
2023-10-05 21:39:32 -0400

    intial spetember speeds

--- data/changeme.js
+++ data/changeme.js
@@ -4,7 +4,7 @@ export const feddate = '9/27/23';
 // changed on the 6th day 
 export const month = 7; 
 // changed on the 4th and 6th day 
-export const is4thday = false;
+export const is4thday = true;
 
 // use this for the graph 
 // changed on the 6th day 

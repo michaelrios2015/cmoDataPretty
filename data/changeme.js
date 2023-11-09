8b5bb9884263dbfe84bf97a451a1aa846465af25
michaelrios2015 <michael.rios093@gmail.com>
2023-11-08 21:12:33 -0500

    6 biz day nov

--- data/changeme.js
+++ data/changeme.js
@@ -1,12 +1,12 @@
 // use these for cmo and ginnie tables 
 // whenever I change fed data 
-export const feddate = '10/25/23';
+export const feddate = '11/01/23';
 // changed on the 6th day 
-export const month = 8; 
+export const month = 9; 
 // changed on the 4th and 6th day 
-export const is4thday = true;
+export const is4thday = false;
 
 // use this for the graph 
 // changed on the 6th day 
-export const date = 'September 2023';
+export const date = 'October 2023';
 

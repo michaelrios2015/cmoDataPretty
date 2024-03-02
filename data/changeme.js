7f2fbf4bdf7789ef604bb4ced6870efa3fc44b04
michaelrios2015 <michael.rios093@gmail.com>
2024-03-02 13:07:58 -0500

    change fed holdings date

--- data/changeme.js
+++ data/changeme.js
@@ -1,6 +1,6 @@
 // use these for cmo and ginnie tables 
 // whenever I change fed data 
-export const feddate = '2/7/24';
+export const feddate = '2/28/24';
 // changed on the 6th day 
 export const month = 0; 
 // changed on the 4th and 6th day 

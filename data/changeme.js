2e2e33ecf80f0c0a4f046f956098e8d588c85fe4
michaelrios2015 <michael.rios093@gmail.com>
2024-07-09 20:57:01 -0400

    6th biz day of july

--- data/changeme.js
+++ data/changeme.js
@@ -1,12 +1,12 @@
 // use these for cmo and ginnie tables 
 // whenever I change fed data 
-export const feddate = '6/05/24';
+export const feddate = '7/03/24';
 // changed on the 6th day 
-export const month = 4; 
+export const month = 5; 
 // changed on the 4th and 6th day 
-export const is4thday = true;
+export const is4thday = false;
 
 // use this for the graph 
 // changed on the 6th day 
-export const date = 'MAY 2024';
+export const date = 'JUNE 2024';
 

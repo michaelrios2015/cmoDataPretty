091bf928d8b1f8529dee37aaa08b5f7c50ea28c7
michaelrios2015 <michael.rios093@gmail.com>
2024-12-06 03:54:44 -0500

    dec 4 biz day

--- data/changeme.js
+++ data/changeme.js
@@ -4,7 +4,7 @@ export const feddate = '11/06/24';
 // changed on the 6th day 
 export const month = 9; 
 // changed on the 4th and 6th day 
-export const is4thday = false;
+export const is4thday = true;
 
 // use this for the graph 
 // changed on the 6th day 

4c97f9cd1179bf642fa0c29aca72a5614c27d9e9
michaelrios2015 <michael.rios093@gmail.com>
2025-04-08 19:37:55 -0400

    6 biz day

--- data/changeme.js
+++ data/changeme.js
@@ -1,13 +1,13 @@
 // use these for cmo and ginnie tables 
 // whenever I change fed data 
-export const feddate = '3/05/25';
+export const feddate = '4/02/25';
 // changed on the 6th day 
-export const month = 1; 
+export const month = 2; 
 // changed on the 4th and 6th day 
-export const is4thday = true;
+export const is4thday = false;
 
 // use this for the graph 
 // changed on the 6th day 
-export const date = 'FEBRUARY 2025';
+export const date = 'MARCHY 2025';
 
 

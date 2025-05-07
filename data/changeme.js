624c6fc304d2f202050b37f5fc7af3322abb019e
michaelrios2015 <michael.rios093@gmail.com>
2025-05-06 21:22:13 -0400

    4th biz day may

--- data/changeme.js
+++ data/changeme.js
@@ -4,10 +4,10 @@ export const feddate = '4/02/25';
 // changed on the 6th day 
 export const month = 2; 
 // changed on the 4th and 6th day 
-export const is4thday = false;
+export const is4thday = true;
 
 // use this for the graph 
 // changed on the 6th day 
-export const date = 'MARCHY 2025';
+export const date = 'MARCH 2025';
 
 

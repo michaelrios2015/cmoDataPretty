f82b7df9f0b56ee191578e943285e2fc44f0090b
michaelrios2015 <michael.rios093@gmail.com>
2025-05-08 20:44:24 -0400

    6th business day may 2025

--- data/changeme.js
+++ data/changeme.js
@@ -1,13 +1,13 @@
 // use these for cmo and ginnie tables 
 // whenever I change fed data 
-export const feddate = '4/02/25';
+export const feddate = '5/07/25';
 // changed on the 6th day 
-export const month = 2; 
+export const month = 3; 
 // changed on the 4th and 6th day 
-export const is4thday = true;
+export const is4thday = false;
 
 // use this for the graph 
 // changed on the 6th day 
-export const date = 'MARCH 2025';
+export const date = 'APRIL 2025';
 
 
--- data/servervar.js
+++ data/servervar.js
@@ -1,5 +1,5 @@
 // so I really vhang eboth at once???
 
-const currentMonth = '2025-03-01';
+const currentMonth = '2025-04-01';
 
 module.exports = currentMonth;

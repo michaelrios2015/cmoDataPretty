7e7c3a078b5ab7bba0964dacd177cac4733575cf
michaelrios2015 <michael.rios093@gmail.com>
2025-06-10 07:07:57 -0400

    6th nusiness day

--- data/changeme.js
+++ data/changeme.js
@@ -1,13 +1,13 @@
 // use these for cmo and ginnie tables 
 // whenever I change fed data 
-export const feddate = '5/07/25';
+export const feddate = '6/04/25';
 // changed on the 6th day 
-export const month = 3; 
+export const month = 4; 
 // changed on the 4th and 6th day 
-export const is4thday = true;
+export const is4thday = false;
 
 // use this for the graph 
 // changed on the 6th day 
-export const date = 'APRIL 2025';
+export const date = 'MAY 2025';
 
 
